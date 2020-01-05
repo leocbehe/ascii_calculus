6fea06dd63b9e671d0ba19d8fa301d0ae6f39c37
Leo Behe <leobehe@gmail.com>
2020-01-05 06:40:30 -0500

    rough draft

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import sys
 import objects.grid
 
 def main():
-   grid = objects.grid.Grid(30, 50)
+   grid = objects.grid.Grid(60, 80)
    grid.assign_range_values()
-   grid.populate_grid()
+   grid.populate_grid(graph_function)
+   grid.display()
+
+def graph_function(x):
+   return x**(1.7)
 
 if __name__ == "__main__":
    main() 
--- objects/grid.py
+++ objects/grid.py
@@ -7,21 +7,44 @@ class Grid():
       self.body = body
       self.xsize = float(xsize)
       self.ysize = float(ysize)
+      self.xvals = 0
+      self.yvals = 0
 
    def assign_range_values(self):
       # TODO give options for negative ranges
       xstart = 0
       ystart = 0
-      xend = xstart + self.xsize
-      yend = ystart + self.ysize
-      xinc = self.xsize / self.cols
-      yinc = self.ysize / self.rows
-      xvals = [x * xinc for x in range(self.cols)]
-      yvals = [y * yinc for y in range(self.rows)]
+      self.xinc = self.xsize / self.cols
+      self.yinc = self.ysize / self.rows
+      # The yvals list is reversed in its range direction just because of the
+      # default order that for loops would print stuff out. It's just flipped so
+      # it looks right in the printout.
+      self.xvals = [x * self.xinc for x in range(xstart, xstart + self.cols)]
+      self.yvals = [y * self.yinc for y in range(ystart + self.rows-1, ystart-1, -1)]
+      print(len(self.yvals))
 
-   def populate_grid(self):
+   def populate_grid(self, func):
       self.body = [[x for x in range(self.cols)] for y in range(self.rows)]
+      func_values = [func(x) for x in self.xvals]
+      row_index = 0
       for row in self.body:
          for col_index in row:
-            row[col_index] = "-"
-      print(self.body)
+            if self.func_match(row_index, col_index, func_values):
+               row[col_index] = "X"
+            else:
+               row[col_index] = "- "
+         row_index += 1
+
+   def func_match(self, row_index, col_index, func_values):
+      current_func_value = func_values[col_index]
+      current_y_value = self.yvals[row_index]
+      upper_bound = current_y_value + self.yinc / 2
+      lower_bound = current_y_value - self.yinc / 2
+      return ( current_func_value < upper_bound and current_func_value >
+lower_bound)
+
+   def display(self):
+      for row in self.body:
+         for col in row:
+            print("{:3}".format(col), end='')
+         print()
